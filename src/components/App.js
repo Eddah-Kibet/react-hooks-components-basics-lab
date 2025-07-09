16a3ba29b18c6f843a450789619a7edb2935fcba
Eddah kibet <eddahkibet20@gmail.com>
2025-07-09 08:42:51 +0300

    components

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,35 @@
 import React from "react";
 
-function NavBar() {
+function NavBar(props) {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href = "#home">I'm a link</a>
     </nav>
   );
 }
 
-function Home() {
+function Home(props) {
   return (
-    <div id="home">
+    <div id = "home">
       <h1>Home</h1>
     </div>
   );
 }
 
-{/* write an <About> component here */}
-
+function About(props){
+  return (
+    <div id = "about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <NavBar/>
+      <Home/>
+      <About/>
     </div>
   );
 }
